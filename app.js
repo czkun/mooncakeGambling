c3c78e8e979ba0fc0e3a04b00869f0767f021b72
心灵老鸭汤 <779062286@qq.com>
2018-09-10 09:06:32 +0800

    v1.0.2
    增加更新版本提示

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-
 App({
   globalData: {
     file: null,
     backgroundAudioManager: null,
+    updateManager: null
   },
   onLaunch: function () {
+    this._creatBackgroundAudioManager()
+    this._creatUpdateManager()
+  },
+  onShow: function(options) {
+    if(wx.getStorageSync('musicPlay')){
+      this.globalData.backgroundAudioManager.src = this.globalData.file.background_music
+    }
+  },
+  onHide: function() {
+    this.globalData.backgroundAudioManager.pause()
+  },
+  _creatBackgroundAudioManager: function () {
     this.globalData.backgroundAudioManager = wx.getBackgroundAudioManager()
     this.globalData.backgroundAudioManager.title = '背景音乐'
     this.globalData.backgroundAudioManager.epname = '背景音乐'
@@ -16,12 +28,19 @@ App({
       }
     })
   },
-  onShow: function(options) {
-    if(wx.getStorageSync('musicPlay')){
-      this.globalData.backgroundAudioManager.src = this.globalData.file.background_music
-    }
-  },
-  onHide: function() {
-    this.globalData.backgroundAudioManager.pause()
+  _creatUpdateManager: function () {
+    this.updateManager = wx.getUpdateManager()
+    this.updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
   }
 })
